236b5cae10465e6284580f3a94f957b6205d06da
Sebastian Heinrich <31190836+naitszox@users.noreply.github.com>
2019-10-03 21:29:49 +0200

    change to tkinter

--- hel.py
+++ hel.py
@@ -1,4 +1,10 @@
-import pyperclip
+from tkinter import Tk, Frame, BOTH, Menu, TclError, Label, RAISED, SUNKEN, SOLID, messagebox
 
-pyperclip.copy('<h1>copypaste test</h1>')
-pyperclip.paste()
+
+
+r = Tk()
+r.withdraw()
+r.clipboard_clear()
+r.clipboard_append('i can has clipboardz?')
+r.update() # now it stays on the clipboard after the window is closed
+r.destroy()
