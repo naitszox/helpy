823c84ad28385f7cd90a293a268247cb78d896eb
Sebastian Heinrich <31190836+naitszox@users.noreply.github.com>
2019-10-03 21:34:30 +0200

    Update hel.py

--- hel.py
+++ hel.py
@@ -1,10 +1,9 @@
 from tkinter import Tk, Frame, BOTH, Menu, TclError, Label, RAISED, SUNKEN, SOLID, messagebox
 
 
-
 r = Tk()
 r.withdraw()
 r.clipboard_clear()
-r.clipboard_append('i can has clipboardz?')
+r.clipboard_append('<123 "dwada" ²1! dwaAd')
 r.update() # now it stays on the clipboard after the window is closed
 r.destroy()
